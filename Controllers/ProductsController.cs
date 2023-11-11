970878d8100542c7e2c681c4e1964f65eaf47444
Luciano Gomes <lgomesroc@github.com>
2023-11-11 08:00:24 -0300

    Listar produtos em uma página Http

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+
+namespace ProductsApp.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1},
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M},
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M}
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        
+        [Microsoft.AspNetCore.Mvc.HttpGet("{id}")]   
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+
+            if (product == null)
+            {
+                return (IHttpActionResult)NotFound();
+            }
+
+            return (IHttpActionResult)Ok(product);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductsApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { set; get; }
+        public decimal Price { set; get; }
+    }
+}
